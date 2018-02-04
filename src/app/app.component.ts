84cc7fee039778052c5dc22033c8148eaa976fc6
unknown <borawaketushar21@gmail.com>
2018-02-04 06:10:58 -0800

    msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Iemployee} from './iemployee/iemployee.component';
+import{EmployeeService} from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'app';
+  constructor(private _service:EmployeeService){}
+  Employee:Iemployee[];
+   ngOnInit() {
+     this.Employee = this._service.getEmployee();
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { Iemployee } from './iemployee/iemployee.component';
+import{EmployeeService} from './employee.service';
+import{UserService} from './user.service';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Iemployee,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpModule
   ],
-  providers: [],
+  providers: [EmployeeService,UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
